e0bcaae389ac2637f69f6c68701b5ad5c2e6a449
Jialue Chen <jialuechen@outlook.com>
2024-08-07 10:02:07 -0400

    update versions

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='tfq-finance',
-    version='1.0.1',
+    version='1.0.2',
     description='A quantum machine learning library for quantitative finance built on top of TensorFlow Quantum',
     author='jialuechen',
     author_email='jialuechen@outlook.com',
     url='https://github.com/jialuechen/tfq-finance',
     packages=find_packages(),
     install_requires=[
-        'tensorflow==2.15.0',
+        'tensorflow==2.11.0',
         'tensorflow-quantum',
         'cirq',
         'numpy',
@@ -20,9 +20,9 @@ setup(
         'scikit-learn',
     ],
     classifiers=[
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.7',
 )
